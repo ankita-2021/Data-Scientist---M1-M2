14f2c9752ef91458e6866d91a66399a2a322cafb
Ankita Chakrawarti <80532826+ankita-2021@users.noreply.github.com>
2026-07-31 14:20:50 +0530

    Create activity10.py

---
+++ activity10.py
@@ -0,0 +1,17 @@
+# Armstrong Number Check
+num = int(input("Enter a number: "))
+
+# Convert to string to find number of digits
+temp = num
+digits = len(str(num))
+sum_of_powers = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum_of_powers += digit ** digits
+    temp //= 10
+
+if sum_of_powers == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
